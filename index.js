ceb36a71c9e2a8bb9aa298b475c10a296aeccb78
Charlie <front-end@wolfox.co>
2021-06-16 16:02:16 +0200

    final version

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+const express = require('express');
+const request = require('request');
+var cors = require('cors')
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', cors(), (req, res) => {
+  request(
+    { url: 'https://dog.ceo/api/breeds/list/all' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+
+      console.log(res.json(JSON.parse(body)))
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
