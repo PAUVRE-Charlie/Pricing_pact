1bddcd6c746eb7977d5d594bebde409ce222a810
Charlie <front-end@wolfox.co>
2021-06-18 15:14:43 +0200

    step 4 completed

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ async function getData() {
             // console.log(responseJSONNoWhile)
             const posts = responseJSONNoWhile.payload.value.posts;
             const users = responseJSONNoWhile.payload.references.User;
-            console.log(users)
             const postsWithEssentialData = posts.map(post => ({id: post.id, author: users[post.creatorId].name, description: post.title, publicationDate: new Date(post.firstPublishedAt)}))
-            console.log(postsWithEssentialData)
             fs.writeFile('./posts.json', JSON.stringify(postsWithEssentialData), 'utf8', (err) => {
                 if (err) {
                     console.log(`Error writing file: ${err}`);
@@ -21,6 +19,7 @@ async function getData() {
                     console.log(`File is written successfully!`);
                 }
             });
+            console.log("\n\n")
         })
         
     } catch (error) {
@@ -28,4 +27,40 @@ async function getData() {
     }
 }
 
+async function getPostsUntil(maxArticle) {
+
+    let postsList = []
+    const pageSize = 10;
+
+    for(let i = 0; i<maxArticle/pageSize; i++){
+        try {
+            await axios.get(`https://medium.com/search/posts?q=pricing${postsList.length > 0 ? `&count=${pageSize}` : ''}${postsList.map(post => `&ignore=${post.id}`)}`, {
+            } ).then((response) => {
+                const responseJSONNoWhile = JSON.parse(response.data.substring(16));
+                const posts = responseJSONNoWhile.payload.value;
+                const users = responseJSONNoWhile.payload.references.User;
+                const postsWithEssentialData = posts.map(post => ({id: post.id, author: users[post.creatorId].name, description: post.title, publicationDate: new Date(post.firstPublishedAt)}))
+                postsList = postsList.concat(postsWithEssentialData)
+            })
+            
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    console.log(postsList)
+
+    fs.writeFile('./postsInfiniteScroll.json', JSON.stringify(postsList), 'utf8', (err) => {
+        if (err) {
+            console.log(`Error writing file: ${err}`);
+        } else {
+            console.log(`File is written successfully!`);
+        }
+    });
+    console.log("\n\n")
+}
+
 getData()
+
+
+getPostsUntil(30)
